3bc77df0cf827dc2c6596ada525349918475b0fb
mayksc96 <mayksc96@gmail.com>
2024-09-24 19:45:20 -0500

    este si

---
+++ src/main/java/com/mycompany/prueba1/Prueba1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba1;
+
+/**
+ *
+ * @author mayks
+ */
+public class Prueba1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
